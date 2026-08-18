14c0c65d7a21cab5ad6f48c8b24217f927183cac
sessentaeseis <66entreaspasduplas@gmail.com>
2026-08-18 08:14:57 -0300

    rotas get

--- src/server.js
+++ src/server.js
@@ -8,5 +8,29 @@ app.use(express.json())
 app.use(cors())
 
 app.get("/equipes", async (req, res) => {
-    
+    try {
+        const equipes = await prisma.equipe.findMany()
+        return res.json(equipes)
+    } catch(error) {
+        res.status(500).json({error: "ERRO ao buscar alunos."})
+    }
+})
+
+app.get("/equipes/:id/desenvolvedores", async (req, res) => {
+    try {
+        const { id } = req.params
+        const desenvolvedores = await prisma.desenvolvedor.findMany({
+            where: {
+                equipeId: Number(id)
+            }
+        })
+        return res.json(desenvolvedores)
+    } catch(error) {
+        res.status(500).json({error: "ERRO ao buscar desenvolvedores."})
+    }
+})
+
+
+app.listen(PORT, () => {
+    console.log("api rodando")
 })
